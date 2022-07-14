52ff889fab96cff84ab4eaf059cc75a9375ea170
Syamim Anuwar <syamim@bizimpulse.io>
2022-07-14 15:50:46 +0800

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { CONFIG } from 'config/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(CONFIG.apiServer.port);
+
+  new Logger('Bootstrap').verbose(
+    "Application is running on: ${await app.getUrl()}",
+  );
+}
+bootstrap();
